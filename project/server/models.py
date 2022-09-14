48bed9bf461e2c70d8d18e67a2982db5cd416ead
shaolin-x <xshaolin123@gmail.com>
2022-09-14 02:29:46 -0400

    finished register

--- project/server/models.py
+++ project/server/models.py
@@ -49,7 +49,7 @@ class User(db.Model):
         :return: integer|string
         """
         try:
-            payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
+            payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'), algorithms=["HS256"])
             return payload['sub']
         except jwt.ExpiredSignatureError:
             return 'Signature expired. Please log in again.'
